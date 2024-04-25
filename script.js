bba3e3f6d109a9dd22d9649e6916d74012415f85
Zetsa <Yves.vonallmen@gmail.com>
2024-04-25 11:13:58 +0200

    add second function

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ form.onsubmit = function(event) {
     form.reset();
     return false;
 }
+
+// Check for blank form field, if true throw error message, else post feedback comment.
+
+function postfeedBack() {
+    let textArea = document.querySelector("textarea");
+    let inputFirstName = document.querySelector("#first-name");
+    let inputLastName = document.querySelector("#last-name");
+
+    if (!inputFirstName.value.trim() || !inputLastName.value.trim() || !textArea.value.trim()) {
+    error.style.display = "block";
+        } else {
+            error.style.display = "none";
+            feedBack();
+        }
+    }
